e885f278e8ff71fa1ee7caaea53e766484884421
Aleksey Arabchik <aleksey-090@mail.ru>
2023-06-08 21:33:25 +0300

    proxy fix

--- src/services/CataloguesService.ts
+++ src/services/CataloguesService.ts
@@ -3,12 +3,12 @@ import axios from "axios";
 export default class CataloguesService {
   static async getCatalogues() {
     const response = await axios.get(
-      "proxy-server-for-jobs-website.vercel.app/2.0/catalogues/",
+      "https://proxy-server-for-jobs-website.vercel.app/2.0/catalogues/",
       {
         headers: { "x-secret-key": "GEU4nvd3rej*jeh.eqp" },
       }
     );
-    
+
     return response.data;
   }
 }
